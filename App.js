e6358456f26146ad7979d08f57d7ba30137b74ca
Muhammad Muhajir <muhajirframe@gmail.com>
2023-02-21 10:28:31 +0400

    add code example

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { Button, StyleSheet, View } from 'react-native'
+import {
+  useAuthRequest,
+  exchangeCodeAsync,
+  revokeAsync,
+} from 'expo-auth-session'
+import * as WebBrowser from 'expo-web-browser'
+import { clientId, redirectUri, discovery } from './constant'
+
+WebBrowser.maybeCompleteAuthSession()
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [authTokens, setAuthTokens] = React.useState(null)
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId,
+      scopes: ['openid', 'offline', 'bookmark'],
+      redirectUri,
+      usePKCE: true,
+    },
+    discovery
+  )
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  React.useEffect(() => {
+    const exchangeFn = async (exchangeTokenReq) => {
+      try {
+        const exchangeTokenResponse = await exchangeCodeAsync(
+          exchangeTokenReq,
+          discovery
+        )
+        setAuthTokens(exchangeTokenResponse)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    if (response) {
+      if (response.error) {
+        Alert.alert(
+          'Authentication error',
+          response.params.error_description || 'something went wrong'
+        )
+        return
+      }
+      if (response.type === 'success') {
+        exchangeFn({
+          clientId,
+          code: response.params.code,
+          redirectUri,
+          extraParams: {
+            code_verifier: request.codeVerifier,
+          },
+        })
+      }
+    }
+  }, [discovery, request, response])
+
+  const logout = async () => {
+    console.log({ authTokens })
+    const revokeResponse = await revokeAsync(
+      {
+        clientId: clientId,
+        token: authTokens.refreshToken,
+      },
+      discovery
+    )
+    if (revokeResponse) {
+      setAuthTokens(null)
+    }
+  }
+  console.log('authTokens: ' + JSON.stringify(authTokens))
+
+  return (
+    <View style={{ paddingTop: 200 }}>
+      {authTokens ? (
+        <Button title="Logout" onPress={() => logout()} />
+      ) : (
+        <Button
+          disabled={!request}
+          title="Login"
+          onPress={() => promptAsync()}
+        />
+      )}
+    </View>
+  )
+}
---
+++ constant.js
@@ -0,0 +1,9 @@
+// Endpoint
+export const discovery = {
+  authorizationEndpoint: 'https://oauth2.quran.com/oauth2/auth',
+  tokenEndpoint: 'https://oauth2.quran.com/oauth2/token',
+  revocationEndpoint: 'https://oauth2.quran.com/oauth2/revoke',
+}
+
+export const clientId = 'quran-demo-mobile'
+export const redirectUri = 'exp://192.168.100.222:19000'
